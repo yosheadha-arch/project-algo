c5cc51a8436e0ee660707dcc0e2c233458b93820
dapaapcb <daffaabyanacc4study@gmail.com>
2026-05-05 20:48:49 +0700

    sampe update

--- sistembookinglapanganfutsal.cpp
+++ sistembookinglapanganfutsal.cpp
@@ -85,4 +85,149 @@ bool cekBentrok(string tanggal, string jam) {
 void tambahBooking(){
     cout << "\n===TAMBAH BOOKING===\n";
     cin.ignore(1000, '\n');
+
+    cout << "Nama Tim: ";
+    getline(cin, data[n].namaTim);
+
+    cout << "Tanggal (YYY-MM-DD): ";
+    getline(cin, data[n].tanggal);
+
+    cout << "Jam (contoh 19:00): ";
+    cout << "Durasi (jam): ";
+    cin >> data[n].durasi:
+
+    // validasi
+    if (data[n].durasi <= 0){
+        cout << "Durasi tidak valid!\n";
+        return;
+    }
+
+    // cek bentrok
+    if(cekBentrok(data[n].tanggal, data[n].jam)){
+        cout << "Jadwal sudah dibooking!\n";
+        return;
+    }
+    data[n].harga = data[n].durasi * 100000;
+
+    n++;
+    simpanFile();
+
+    cout << "Booking berhasil ditambahkan!\n";
 }
+
+// sorting
+void sortingAsc() {
+    for (int i = 0; i < n-1; i++) {
+        for (int j = 0; j< n-i-1; j++) {
+            if (data[j].tanggal < data[j+1].tanggal) {
+                swap(data[j], data[j=1]);
+            }
+        }
+    }
+}
+
+void sortingDesc() {
+    for (int i = 0; i < n-1; i){
+        for (int j = 0; j < n-i-1; j++) {
+            if (data[j].tanggal , data[j+1].tanggal) {
+                swap (data[j], data[j+1]);
+            }
+        }
+    }
+}
+
+// tampil
+void tampilBooking() {
+    int pilih;
+
+    bacaFile();
+
+    if(n == 0){
+        cout << "Data kosong.\n";
+        return;
+    }
+
+    cout << "\n1. Ascending\n2. Descending\npilih: ";
+    cin >> pilih;
+    if (pilih == 1)
+        sortingAsc();
+    else
+        sortinngDsc();
+
+    cout << "\n======================================\n";
+    cout << left << setw(15) << "Tim"
+         << setw(15) << "Tanggal"
+         << setw(10) << "Jam"
+         << setw(10) << "Durasi"
+         << setw(10) << "Harga" << endl;
+    cout << "\n======================================\n";
+
+    for (int i = 0; i < n; i++) {
+        cout << setw(15) << data[i].namaTim
+        cout << setw(15) << data[i].tanggal
+        cout << setw(10) << data[i].jam
+        cout << setw(10) << data[i].durasi
+        cout << setw(10) << data[i].harga << endl;
+    }
+}
+
+// search 
+void cariBooking() {
+    string cari;
+    cin.ignore(1000, '\n');
+
+    cout << "\nMAsukan nama tim: ";
+    getline(cin, cari);
+
+    bacaFile();
+    bool ketemu = false;
+
+    for (int i = 0; i < n; i++) {
+        if (data[i].namaTim == cari) {
+            cout << "\nData ditemukan:\n"; 
+            cout << "Tim     : " << data[i].namaTim << endl;
+            cout << "Tanggal : " << data[i].tanggal << endl;
+            cout << "Jam     : " << data[i].jam << endl;
+            cout << "Durasi  : " << data[i].durasi << endl;
+            cout << "Harga   : " << data[i].harga << endl;
+            ketemu = true;
+            break;
+        }
+    }
+
+    if (!ketemu) {
+        cout << "Data tidak ditemukan.\n";
+
+    }
+}
+
+// update
+void updateBooking() {
+    string cari;
+    cin.ignore(1000, '\n')
+
+    cout << "\nMasukan nama tim: ";
+    getline(cin, cari);
+
+    bacaFile();
+    bool ketemu = false;
+
+    for (int i = 0; i < n; i++) {
+        if (data [i].namaTim == cari) {
+            cout << "Durasi baru: ";
+            cin >> data[i].durasi;
+
+            data[i].harga = data[i].durasi * 100000;
+         ketemu = true;
+         break;
+        }
+    }
+
+    if (ketemu) {
+        simpanFile();
+        cout << "Data Berhasil diupdate!\n";
+    } else {
+        cout << "Data tidak ditemukan.\n";
+    }
+}
+
