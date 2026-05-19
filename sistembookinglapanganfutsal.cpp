9acce255a4b858d96fdf34b1ba5bc39b8a8b100e
yosheadha-arch <yosheadha@gmail.com>
2026-05-19 18:30:31 +0700

    struct

---
+++ sistembookinglapanganfutsal.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+using namespace std;
+
+//=======struct=========
+
+struct Booking {
+    string namaTim;
+    string tanggal;
+    string jam;
+    int durasi;
+    int harga;
+}
